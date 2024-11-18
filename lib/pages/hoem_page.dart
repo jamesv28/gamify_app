6ec5ef5c18516f823c5ed3a4d3192966a84a6b12
James Volmert <james.volmert@gmail.com>
2024-11-17 18:52:39 -0700

    first commit

---
+++ lib/pages/hoem_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HoemPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
